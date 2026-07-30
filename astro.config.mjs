b9d6d4ad3181dcd93e8f869674d3ec6d5e617823
maiaenergiasrenovaveis <maiaenergiasrenovaveis@gmail.com>
2026-07-30 20:26:21 -0300

    Corrigir URLs canonicas e sitemap para nao usar barra final, corrigir sitemap no robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://maiaenergiasrenovaveis.com.br',
+  trailingSlash: 'never',
   vite: {
     plugins: [tailwindcss()]
   },
